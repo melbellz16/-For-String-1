4fcfc034f6b63a86922bc97511ff5c2c9e43b9d3
Melbellz16 <89919602+melbellz16@users.noreply.github.com>
2022-04-25 16:55:14 +0000

    добавяем элемент новый в массив Andrew

--- Main.java
+++ Main.java
@@ -5,14 +5,14 @@ class Main {
       age +=  1;
     System.out.println("I'm"+ age + " years old.");
     //Создаем массив данных.String-  обьект
-    String[] names = new String[] { "Josh", "Bill"};
+    String[] names = new String[] { "Josh", "Bill", "Andrew"};
     // Создаем цикл (For), который будет итеррировать значения в массив
     for (String name : names) {
       //String name- название переменной, names-название массива или коллекции
       System.out.println(name);
     }
-    
-
+    //Для каждого элемента массива мы выполняем код,который находится в фигурных скобках
+//Добавляем еще один элемент в массив "Andrew"
     
     }
 }
